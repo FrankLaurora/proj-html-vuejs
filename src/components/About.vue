d2d82066c8c9e18feb44ee576f612bb99179ee24
FrankLaurora <francesco.laurora9@gmail.com>
2021-10-11 21:04:10 +0200

    Fix typo and better font for yellow titles

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="about md_container">
-        <h2>More About Dan Vaughn</h2>
+        <h2>More About Damon Vaughn</h2>
         <p>Damon Vaughn is a professor of psychology at the University of Toronto, a clinical psychologist and the author of the multi-million copy bestseller "The story of my life."</p>
         <button class="btn btn_green">Meet the Author</button>
     </div>
